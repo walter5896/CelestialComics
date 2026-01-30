ad0934e15c352184316a1e9697b3d07333403da8
Walter Blundell <wbblundell333@gmail.com>
2026-01-30 14:24:16 -0700

    fix?

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,8 @@
+// /js/supabase.js
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+// Use keys from the browser-exposed env.js
+const supabaseUrl = window.__env.PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = window.__env.PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
