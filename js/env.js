25b06335d0cdab048ef586d840c73814b24768f1
Walter Blundell <wbblundell333@gmail.com>
2026-02-25 15:25:06 -0700

    DASF

---
+++ js/env.js
@@ -0,0 +1,3 @@
+
+window.PUBLIC_SUPABASE_URL = "https://axkifbrakyboodtxmimb.supabase.co";
+window.PUBLIC_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4a2lmYnJha3lib29kdHhtaW1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwODg5MTEsImV4cCI6MjA4MTY2NDkxMX0.v8pMv2VkGWUNjBDzdbfyuNYiWzkag7aYj4XOGFsjMCs";
